6895e52be91081c0d4de2decbda4e0625785640c
mridene <mridene@iobeya.com>
2022-06-15 15:34:51 +0100

    add main dependencies
    -bootstrap
    - font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <h2>App component</h2>
 </template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
 
 <style lang="less">
 #app {
