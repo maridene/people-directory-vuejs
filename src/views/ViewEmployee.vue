a7ba5f003be7b5bceae288b509c54787d0794a09
mridene <mridene@iobeya.com>
2022-06-16 16:35:08 +0100

    implement view employee page

--- src/views/ViewEmployee.vue
+++ src/views/ViewEmployee.vue
@@ -9,34 +9,51 @@
       </div>
     </div>
   </div>
-  <div class="container">
+
+  <div v-if="loading">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Spinner />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div v-if="!loading && errorMessage">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p class="h4 text-danger fw-bold">{{ errorMessage }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container" v-if="!loading && isValid()">
     <div class="row align-items-center">
       <div class="col-md-4">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/219/219969.png"
-          alt=""
-          class="employee-img-big"
-        />
+        <img :src="employee.photo" alt="" class="employee-img-big" />
       </div>
       <div class="col-md-6">
         <ul>
           <li class="list-group-item">
-            Name : <span class="fw-bold">Name</span>
+            Name : <span class="fw-bold">{{ employee.name }}</span>
           </li>
           <li class="list-group-item">
-            Email : <span class="fw-bold">Email</span>
+            Email : <span class="fw-bold">{{ employee.email }}</span>
           </li>
           <li class="list-group-item">
-            Mobile : <span class="fw-bold">Mobile</span>
+            Mobile : <span class="fw-bold">{{ employee.mobile }}</span>
           </li>
           <li class="list-group-item">
-            Company : <span class="fw-bold">Mobile</span>
+            Company : <span class="fw-bold">{{ employee.company }}</span>
           </li>
           <li class="list-group-item">
-            Title : <span class="fw-bold">Mobile</span>
+            Title : <span class="fw-bold">{{ employee.title }}</span>
           </li>
           <li class="list-group-item">
-            Department : <span class="fw-bold">Mobile</span>
+            Department : <span class="fw-bold">{{ department.name }}</span>
           </li>
         </ul>
       </div>
@@ -53,8 +70,43 @@
 </template>
 
 <script>
+import { EmployeesService } from "@/services/employeesService";
+import Spinner from "@/components/Spinner";
+
 export default {
   name: "ViewEmployee",
+  components: { Spinner },
+  data: function () {
+    return {
+      employeeId: this.$route.params.employeeId,
+      loading: false,
+      employee: {},
+      errorMessage: null,
+      department: {},
+    };
+  },
+  created: async function () {
+    try {
+      this.loading = true;
+      let response = await EmployeesService.getEmployee(this.employeeId);
+      this.employee = response.data;
+      let depResponse = await EmployeesService.getDepartment(
+        this.employee.departmentId
+      );
+      this.department = depResponse.data;
+      this.loading = false;
+    } catch (error) {
+      this.errorMessage = error;
+      this.loading = false;
+    }
+  },
+  methods: {
+    isValid: function () {
+      return (
+        Object.keys(this.employee).length && Object.keys(this.department).length
+      );
+    },
+  },
 };
 </script>
 
